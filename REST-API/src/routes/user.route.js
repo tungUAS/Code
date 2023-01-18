17a897ecf0026785644d09a4c613df9d67945bf1
tungUAS <tung.tt133@gmail.com>
2023-01-18 17:49:26 +0100

    changes

--- REST-API/src/routes/user.route.js
+++ REST-API/src/routes/user.route.js
@@ -3,12 +3,13 @@ var router = express.Router();
 const multer  = require('multer')
 const upload = multer({ storagex: multer.memoryStorage({}) })
 const authMiddleware = require('../middleware/verifySignUp');
+const authJWT = require("../middleware/authJWT");
 const authController = require('../controllers/auth.controller')
 
 router.post('/signup',authMiddleware.checkDuplicateUsernameOrEmail,authController.signup);
 
 router.post('/signin',authController.signin);
 
-router.post('/update-profile', upload.single('image'), authController.updateProfile);
+router.post('/update-profile', upload.single('image'),authJWT.verifyTokenUser, authController.updateProfile);
 
 module.exports = router;
---
+++ readme.md
@@ -0,0 +1,13 @@
+1. Go To GraphQL Folder
+2. Delete node_modules
+3. Reinstall node_modules. please type "npm install" in the cmd
+4. Run on port 5000. please type "node ./bin/www"
+5. Open Browser, run "localhost:5000/graphql"
+
+
+
+1. Go To REST-API Folder
+2. Delete node_modules
+3. Reinstall node_modules. please type "npm install" in the cmd
+4. Run on port 3000. please type "node ./bin/www"
+5. Open Browser, run "localhost:3000/api/v1/books/"
